3b92e02c9dc25b744bba8f09f08d617fb82930b6
igorminash <iminash@inbox.ru>
2023-03-07 20:35:18 +0300

    Добавлены решения

---
+++ src/main/java/org/example/hw1/Ex1.java
@@ -0,0 +1,15 @@
+package org.example.hw1;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String str = scanner.nextLine();
+        String  str2 = str.trim();
+        String [] words = str2.split("\\s+");
+        for (int i = words.length - 1; i >= 0; i--) {
+            System.out.printf(words[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/org/example/hw2/Ex2.java
@@ -0,0 +1,21 @@
+package org.example.hw2;
+
+import java.util.Scanner;
+
+public class Ex2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int b = scanner.nextInt();
+        while (b != 0) {
+            int a = b;
+            b = scanner.nextInt();
+            if (b != 0) {
+                if (a > 0 && b < 0) {
+                    sum += a;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}
